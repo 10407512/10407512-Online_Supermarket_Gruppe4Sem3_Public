40fdf693744ccb65360a6a4949f8e055b7c27cc4
10407512 <10407512@ucn.dk>
2023-02-10 10:46:36 +0100

    Initialize repository from private repository

---
+++ DataAccess/DAOs/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace DataAccess.DataAccess
+{
+    public class DBConnection
+    {
+        protected readonly IConfiguration _configuration;
+        public DBConnection()
+        {
+            _configuration = new ConfigurationBuilder().SetBasePath(Path.GetFullPath("../DataAccess")).AddJsonFile("config.json").Build();
+        }
+
+        public static SqlConnectionStringBuilder _connectionStringBuilder = new()
+        {
+            
+        };
+    }
+}
